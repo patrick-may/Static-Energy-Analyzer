c933aa0e12ba513b5fb07823bd6b7dd771d62dbf
patrick-may <pmay24@wooster.edu>
2024-02-11 14:50:02 -0500

    Testing PI assembler along with stress tester

--- TestPrograms/array.s
+++ TestPrograms/array.s
@@ -7,35 +7,14 @@
 main:
 .LFB0:
 	.cfi_startproc
-	stp	x29, x30, [sp, -256]!
-	.cfi_def_cfa_offset 256
-	.cfi_offset 29, -256
-	.cfi_offset 30, -248
-	mov	x29, sp
-	adrp	x0, :got:__stack_chk_guard
-	ldr	x0, [x0, #:got_lo12:__stack_chk_guard]
-	ldr	x1, [x0]
-	str	x1, [sp, 248]
-	mov	x1,0
+	sub	sp, sp, #336
+	.cfi_def_cfa_offset 336
 	mov	w0, 0
-	mov	w1, w0
-	adrp	x0, :got:__stack_chk_guard
-	ldr	x0, [x0, #:got_lo12:__stack_chk_guard]
-	ldr	x2, [sp, 248]
-	ldr	x3, [x0]
-	subs	x2, x2, x3
-	mov	x3, 0
-	beq	.L3
-	bl	__stack_chk_fail
-.L3:
-	mov	w0, w1
-	ldp	x29, x30, [sp], 256
-	.cfi_restore 30
-	.cfi_restore 29
+	add	sp, sp, 336
 	.cfi_def_cfa_offset 0
 	ret
 	.cfi_endproc
 .LFE0:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0"
+	.ident	"GCC: (Debian 12.2.0-14) 12.2.0"
 	.section	.note.GNU-stack,"",@progbits
